47f3beead819ad486f1c73daee1dc36e13fa0f3a
Myles Gray <mg@mylesgray.com>
2020-12-03 12:26:05 +0000

    Fat fingered var

--- scripts/tf_cnn_benchmarks/benchmark_cnn.py
+++ scripts/tf_cnn_benchmarks/benchmark_cnn.py
@@ -964,7 +964,7 @@ def get_perf_timing_str(speed_mean, speed_uncertainty, speed_jitter, scale=1):
     r.status_code
 
     # Report SMI status to prometheus
-    smi=nvsmi.getgpus()
+    smi=nvsmi.get_gpus()
     print(smi)
     #gpu_utilisation.set(smi.)
     #gpu_mem_used.set()
